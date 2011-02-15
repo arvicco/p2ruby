7be0470643ea90c00f049473b074dded625890ab
arvicco <arvitallian@gmail.com>
2011-02-15 14:19:41 +0300

    Logging setup

--- spec/p2ruby/application_spec.rb
+++ spec/p2ruby/application_spec.rb
@@ -7,6 +7,6 @@ describe P2Ruby::Application do
     subject.ole_type.name.should == 'IP2Application'
     print 'Implemented OLE types: '; p subject.ole_type.implemented_ole_types
     print 'Source OLE types: '; p subject.ole_type.source_ole_types
-    print 'OLE methods:' ; p (subject.ole_methods - Object.methods).map &:name
+    print 'OLE methods: '; p (subject.ole_methods - Object.methods).map &:name
   end
 end
