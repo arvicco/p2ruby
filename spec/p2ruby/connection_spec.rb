14d99ba1c437db5d69eacb30efaa6d11d2e542cf
arvicco <arvitallian@gmail.com>
2011-02-17 19:51:24 +0300

    More Connection methods spec'ed

--- spec/p2ruby/connection_spec.rb
+++ spec/p2ruby/connection_spec.rb
@@ -52,82 +52,141 @@ describe P2Ruby::Connection do
       its(:LoginStr) { should == "Blah" }
       it_behaves_like 'new connection'
     end
+  end
 
-    describe '#Connect()', 'creates local connection to Router' do
-      context 'with correct connection parameters' do
-        it 'connects successfully' do
-          @conn = P2Ruby::Connection.new :app_name => random_name,
-                                        :host => "127.0.0.1", :port => 4001
-          @conn.Connect().should == P2::P2ERR_OK
-          @conn.NodeName.should == ROUTER_LOGIN
-          @conn.should be_connected
-          @conn.status_text.should == "Connection Connected, Router Connected"
-        end
-      end
-
-      context 'with wrong connection parameters' do
-        it 'fails to connect' do
-          @conn = P2Ruby::Connection.new :app_name => random_name, :timeout => 200,
-                                        :host => "127.0.0.1", :port => 1313
-          expect { @conn.Connect() }.to raise_error /Couldn't connect to MQ/
-          @conn.should_not be_connected
-          @conn.status_text.should == "Connection Disconnected"
-        end
+  describe '#Connect()', 'creates local connection to Router' do
+    context 'with correct connection parameters' do
+      it 'connects successfully' do
+        @conn = P2Ruby::Connection.new :app_name => random_name,
+                                       :host => "127.0.0.1", :port => 4001
+        @conn.Connect().should == P2::P2ERR_OK
+        @conn.NodeName.should == ROUTER_LOGIN
+        @conn.should be_connected
+        @conn.status_text.should == "Connection Connected, Router Connected"
       end
     end
 
-    describe '#Disconnect()', 'drops local connection to Router' do
-      context 'when connected to Router' do
-        before do
-          @conn = P2Ruby::Connection.new :app_name => random_name,
-                                         :host => "127.0.0.1", :port => 4001
-          @conn.Connect()
-          @conn.should be_connected
-        end
+    context 'with wrong connection parameters' do
+      it 'fails to connect' do
+        @conn = P2Ruby::Connection.new :app_name => random_name, :timeout => 200,
+                                       :host => "127.0.0.1", :port => 1313
+        expect { @conn.Connect() }.to raise_error /Couldn't connect to MQ/
+        @conn.should_not be_connected
+        @conn.status_text.should == "Connection Disconnected"
+      end
+    end
+  end
 
-        it 'disconnects from Router successfully' do
-          @conn.Disconnect()
-          expect { @conn.NodeName }.to raise_error /Couldn't get MQ node name/
-          @conn.should_not be_connected
-          @conn.status_text.should == "Connection Disconnected"
-        end
+  describe '#Disconnect()', 'drops local connection to Router' do
+    context 'when connected to Router' do
+      before do
+        @conn = P2Ruby::Connection.new :app_name => random_name,
+                                       :host => "127.0.0.1", :port => 4001
+        @conn.Connect()
+        @conn.should be_connected
       end
 
-      context 'when NOT connected to Router' do
-        it 'it`s noop' do
-          @conn = P2Ruby::Connection.new :app_name => random_name,
-                                         :host => "127.0.0.1", :port => 1313
-          @conn.Disconnect()
-          @conn.should_not be_connected
-        end
+      it 'disconnects from Router successfully' do
+        @conn.Disconnect()
+        expect { @conn.NodeName }.to raise_error /Couldn't get MQ node name/
+        @conn.should_not be_connected
+        @conn.status_text.should == "Connection Disconnected"
       end
     end
 
-    describe '#Logout(void)', 'drops Router connection to uplink (RTS)' do
-      context 'when Router is connected' do
-        before do
-          @conn = P2Ruby::Connection.new :app_name => random_name,
-                                         :host => "127.0.0.1", :port => 4001
-          @conn.Connect()
-          @conn.should be_connected
-        end
-
-        xit 'disconnects Router from uplink' do
-          @conn.Logout()
-          sleep 4
-          @conn.status_text.should == "Connection Disconnected"
-        end
-
-        xit 'but keeps local connection to Router' do
-          @conn.Logout()
-          @conn.should be_connected
-          @conn.status_text.should == "Connection Disconnected"
-        end
+    context 'when NOT connected to Router' do
+      it 'it`s noop' do
+        @conn = P2Ruby::Connection.new :app_name => random_name,
+                                       :host => "127.0.0.1", :port => 1313
+        @conn.Disconnect()
+        @conn.should_not be_connected
       end
+    end
+  end
 
+  describe '#Connect2 '
+  #  Connect2 ( [in] BSTR connStr, [out, retval] ULONG* errClass);
+  #  —оздание локального соединени€ приложени€ с роутером. ¬ыпущен в дополнение к методу Connection.Connect.
+
+  describe '#ProcessMessage'
+  #  ProcessMessage ( [out] ULONG* cookie,  [in] ULONG pollTimeout);
+  #  ѕрием и обработка сообщений, в том числе и репликационных.
+  #  јргументы
+  #  Х	pollTimeout Ч таймаут в миллисекундах, в течение которого ожидаетс€ получение сообщени€;
+  #  Х	cookie Ч уникальный идентификатор подписчика.
+
+  describe '#ProcessMessage2'
+  #  ProcessMessage2 ( [in] ULONG pollTimeout, [out, retval] ULONG* cookie);
+  #  ѕрием и обработка сообщений. ¬ыпущен в дополнение к методу Connection.ProcessMessage,
+  #  так как тот не позвол€л в интерпретированных €зыках (JScript) получить результат работы функции (cookie).
+
+  describe '#RegisterReceiver'
+  #  RegisterReceiver ( [in] IP2MessageReceiver* newReceiver, [out,retval] ULONG* cookie);
+  #  –егистраци€ подписчика.
+  #  јргументы
+  #  Х	newReceiver Ч указатель на интерфейс обратного вызова;
+  #  Х	cookie Ч уникальный идентификатор подписчика. »спользуетс€ дл€ того, чтобы можно
+  #     было отменить подписку, а также именно по нему в методе Connection.ProcessMessage
+  #     определ€етс€ получатель сообщени€.
+
+  describe '#UnRegisterReceiver'
+  #  UnRegisterReceiver ([in] ULONG cookie);
+  #  ќтмена регистрации подписчика.
+
+  describe '#ResolveService' do
+    #  ResolveService ( [in] BSTR service, [out,retval] BSTR* address);
+    #  ѕолучение полного адреса приложени€ по имени сервиса, который оно предоставл€ет.
+    #  јргументы
+    #  Х	service Ч им€ сервиса;
+    #  Х	address Ч полный адрес приложени€.
+
+    before(:all) do
+      @conn = P2Ruby::Connection.new :app_name => random_name,
+                                     :host => "127.0.0.1", :port => 4001
+      @conn.Connect()
+      @conn.should be_connected
+      @conn.should be_logged
     end
 
+    it 'returns full server address by service name' do
+      @conn.ResolveService('FORTS_OPTINFO_REPL').should == "FINTER_FORTS3.inter_info"
+      @conn.ResolveService('FORTS_FUTINFO_REPL').should == "FINTER_FORTS3.inter_info"
+      @conn.ResolveService('FORTS_POS_REPL').should == "FINTER_FORTS3.inter_pos"
+      @conn.ResolveService('FORTS_FUTCOMMON_REPL').should== "FINTER_FORTS3.inter_futcommon"
+      @conn.ResolveService('FORTS_OPTCOMMON_REPL').should== "FINTER_FORTS3.inter_optcommon"
+      @conn.ResolveService('FORTS_VOLAT_REPL').should == "FINTER_FORTS3.inter_vmv"
+      @conn.ResolveService('FORTS_VM_REPL').should == "FINTER_FORTS3.inter_vmv"
+      # Order placement:
+      @conn.ResolveService("FORTS_SRV").should == "FINTER_FORTS3.Dispatcher"
+    end
+  end
 
+  #	јутентификаци€ выполн€етс€ асинхронно. ”спешный возврат из метода Login означает,
+  # что аутентификационна€ информаци€ была послана на сервер. ƒл€ того, чтобы узнать,
+  # успешно ли завершилась аутентификаци€, следует получать и обрабатывать уведомлени€
+  # о состо€нии соединени€.
+  context 'When router is authenticated via ini file' do
+  end
+  context 'When router is authenticated explicitely - via Login()' do
+  end
 
+  describe '#Logout(void)', 'drops Router connection to uplink (RTS)' do
+    context 'when Router is connected' do
+      before(:all) do
+        @conn = P2Ruby::Connection.new :app_name => random_name,
+                                       :host => "127.0.0.1", :port => 4001
+        @conn.Connect()
+        @conn.should be_connected
+        @conn.Logout()
+      end
+
+      it 'keeps local connection to Router' do
+        @conn.should be_connected
+      end
+
+      it 'only sends an ASYNC request to disconnect Router from uplink' do
+        @conn.should be_logged # Need to wait for Server to react to logout
+      end
+    end
   end
 end
